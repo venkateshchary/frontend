ee719bbf0ff4848fdc39bcd031520e8496167de2
VENKATESH <vchary78@gmail.com>
2026-04-22 05:24:12 +0530

    props implementation

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
 import './App.css'
 import CompanyCard from './components/CompanyCard.tsx'
 
 
 function App() {
+  const companyDetails=[{name:'Zelis Healthcare', duration:'2023-Present'},
+                        {name:'Apree Health', duration:'2021-2023'}
+                      ];
+  //<CompanyCard companyDetails={{name:'Zelis Healthcare', duration:'2023-Present'}} />
   return (
     <>
-    <CompanyCard companyDetails={{name:'Apree Health', duration:'2021-2023'}} />
-    <CompanyCard companyDetails={{name:'Zelis Healthcare', duration:'2023-Present'}} />
+      {
+        companyDetails.map((company) =>(
+           <CompanyCard companyDetails={company} key={company.name} />
+        ))
+      }
     </>
-  )
+  );
 }
 
 export default App
