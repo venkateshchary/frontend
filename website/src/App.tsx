72374ccd00a972605a612f1ff21f826b84e48adf
VENKATESH <vchary78@gmail.com>
2026-04-23 18:18:13 +0530

    add: images

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,20 +2,37 @@ import './App.css'
 import CompanyCard from './components/CompanyCard.tsx';
 import UserInfoCard from './components/UserInfoCard.tsx';
 import WelcomeCard from './components/WelcomeCard.tsx';
+import user_logo from './assets/img/alen.jpg'; // Import image
+import apree_logo from './assets/img/Apree_Health_Logo.jpg'; // Import image
+import enlume_logo from './assets/img/enlume_logo.jpg'; // Import image
+import zelis_logo from './assets/img/Zelis-logo-banner.jpg'; // Import image
+import mobigesture_logo from './assets/img/mobigesture_logo.jpg'; // Import image
+
+
+
 
 
 function App() {
   const companyDetails = [{
-    name: 'Zelis Healthcare', duration: '2023-Present',
+    name: 'Zelis Healthcare', duration: '2023-Present', logo: zelis_logo,
     projectDetails: [{
       framework: 'Django', cloud: 'AWS', projectName: 'Guidance Engine'
     }
     ]
   },
   {
-    name: 'Apree Health', duration: '2021-2023',
+    name: 'Apree Health', duration: '2021-2023', logo: apree_logo,
     projectDetails: [{ framework: 'Django', cloud: 'GCP', projectName: 'Admin Portal' },
-    { framwork: 'Flask', cloud: 'GCP', projectName: 'Files To BQ' }]
+    { framework: 'Flask', cloud: 'GCP', projectName: 'Files To BQ' }]
+  },
+  {
+    name: 'Enlume Technologies', duration: '2019-2021', logo: enlume_logo,
+    projectDetails: [{ framework: 'Chalice & Zappa', cloud: 'AWS', projectName: 'Monarch Tractors' }]
+  },
+  {
+    name: 'Mobigesture Software Pvt Ltd', duration: '2016-2019', logo: mobigesture_logo,
+    projectDetails: [{ framework: 'Django', cloud: 'AWS', 'projectName': 'WebPR' },
+    { framework: '', projectName: 'VCU TRD' }]
   }
   ];
   return (
@@ -24,7 +41,11 @@ function App() {
       <UserInfoCard />
       {
         companyDetails.map((company) => (
-          <CompanyCard name={company.name} duration={company.duration} projectDetails={company.projectDetails} key={company.name} />
+          <CompanyCard name={company.name}
+            duration={company.duration}
+            projectDetails={company.projectDetails}
+            logo={company.logo}
+            key={company.name} />
         ))
       }
     </>
